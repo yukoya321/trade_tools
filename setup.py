167a075563f48cf64814f6aa3ecf9c73f1f4111c
y.koya <kamo.kamonohashi135@gmail.com>
2018-12-20 19:52:22 +0900

    modify dir

---
+++ setup.py
@@ -0,0 +1,11 @@
+setup(
+    name='sample',
+    version='0.0.1',
+    description='Sample package',
+    long_description=readme,
+    author='',
+    author_email='',
+    url='',
+    license=license,
+    packages=find_packages()
+)
---
+++ trade_tools/core.py
@@ -0,0 +1,7 @@
+import settings
+
+API_URL = settings.URL
+API_KEY = settings.API
+
+print(API_URL)
+print(API_KEY)
